85093bda6fde096d6fa33c247184f0d9a3069c03
Sarvottam <sarvottamkumar415@gmail.com>
2022-08-18 07:52:37 +0530

    majority init

--- divideAndConquer/searching.go
+++ divideAndConquer/searching.go
@@ -41,4 +41,45 @@ func binarySearch(inputSlice []int, startingIdex int, LastIndex int, number int)
 
 }
 
+// A element in given array said to be majority element if it
+// appear in array more than n/2 times.
+func CheckMajoryExit(inputSlice []int) int {
 
+	lengthOfArray := len(inputSlice)
+	mid := lengthOfArray - 1/2
+
+	midElement := inputSlice[mid]
+
+	startIndex := findStartIndex(inputSlice, midElement, 0, mid-1)
+	lastIndex := findLastINdex(inputSlice, midElement, mid+1, lengthOfArray-1)
+
+	if (lastIndex - startIndex) > lengthOfArray/2 {
+		return midElement
+	}
+	return -1
+
+}
+
+func findStartIndex(inputSlice []int, midElement, firstIndex, lastIndex int) int {
+
+	startIndex := -1
+	mid := firstIndex + lastIndex/2
+	if inputSlice[mid] == midElement {
+		if inputSlice[mid-1] == midElement {
+			startIndex = findStartIndex(inputSlice, midElement, firstIndex, mid-1)
+		} else if inputSlice[mid+1] == midElement {
+			startIndex = mid
+		}
+
+	} else {
+		startIndex = findStartIndex(inputSlice, midElement, mid+1, lastIndex)
+	}
+
+	return startIndex
+
+}
+func findLastINdex(inputSlice []int, midElement, startIndex, lastIndex int) int {
+
+	return -1
+
+}
